6788c8dfeae129d2b94d383d5e280114c26807aa
rachidfrex <2003043000250@ofppt-edu.ma>
2024-05-15 17:11:09 +0100

    chore: Update README with React + Vite template details

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Nav from './components/nav'
+function App() {
+
+  return (
+    <div className='relative w-full flex justify-center items-center'>
+       <Nav />
+    </div>
+  )
+}
+
+export default App
